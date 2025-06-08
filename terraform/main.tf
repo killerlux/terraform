d10ae60fab95ffe08f6643cfcdad98ffd7b876be
Aymen Kouskoussi <aymen.kouskoussi@gmail.com>
2025-06-09 00:05:30 +0200

    Refactor: Install Docker via SSH and clone repo on server

--- terraform/main.tf
+++ terraform/main.tf
@@ -25,18 +25,10 @@ resource "digitalocean_droplet" "private_ai_server" {
   # IMPORTANT : Remplacez la valeur entre crochets par l'ID de votre clé SSH sur DigitalOcean.
   ssh_keys = ["62:69:4b:04:86:c4:bc:41:4a:e5:96:08:69:3d:d3:54"]
 
-  # Script exécuté à la création du Droplet pour installer Docker et Docker Compose
+  # Script minimaliste, Docker sera installé par le workflow
   user_data = <<-EOF
               #!/bin/bash
               apt-get update -y
-              apt-get install -y apt-transport-https ca-certificates curl software-properties-common
-              curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
-              echo "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
-              apt-get update -y
-              apt-get install -y docker-ce docker-ce-cli containerd.io docker-compose-plugin
-              usermod -aG docker ubuntu
-              systemctl restart docker
-              sleep 10
               ufw allow ssh
               ufw allow 5678/tcp
               ufw --force enable
