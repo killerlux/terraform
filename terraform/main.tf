b7f4134af382cdd9c5b1f1bd42f2c08d98f72958
Aymen Kouskoussi <aymen.kouskoussi@gmail.com>
2025-06-15 03:01:15 +0200

    feat: update Droplet size to s-4vcpu-8gb for better model performance

--- terraform/main.tf
+++ terraform/main.tf
@@ -20,10 +20,10 @@ resource "digitalocean_droplet" "private_ai_server" {
   image  = "ubuntu-22-04-x64" # Utilise une image Ubuntu stable
   name   = "private-ai-server-01"
   region = "fra1" # Francfort, à changer si nécessaire
-  size   = "s-2vcpu-4gb" # Un bon point de départ.
+  size   = "s-4vcpu-8gb" # Augmenté pour Llama2
 
   # IMPORTANT : Remplacez la valeur entre crochets par l'ID de votre clé SSH sur DigitalOcean.
-  ssh_keys = ["62:69:4b:04:86:c4:bc:41:4a:e5:96:08:69:3d:d3:54"]
+  ssh_keys = ["48478391"]
 
   # Script minimaliste, Docker sera installé par le workflow
   user_data = <<-EOF
